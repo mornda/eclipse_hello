22801f439d8a90da66a8b6cdb458c40b71a3bf63
mornda <junier245@daegu.ac.kr>
2023-04-03 11:34:52 +0900

    added 'your world'

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello his World");
-		System.out.println("Hello your World");
+		System.out.println("Hello your world");
 
 	}
 
